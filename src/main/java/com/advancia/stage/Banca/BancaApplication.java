aec509b79643ed7dc87017daf738ea54e33e5880
Salvatore Bisconti <121030864+SalvoBisconti@users.noreply.github.com>
2025-02-13 12:48:17 +0100

    build

--- src/main/java/com/advancia/stage/Banca/BancaApplication.java
+++ src/main/java/com/advancia/stage/Banca/BancaApplication.java
@@ -2,12 +2,20 @@ package com.advancia.stage.Banca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BancaApplication {
+public class BancaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BancaApplication.class, args);
 	}
+	
+	// METODO PER IL BUILD
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BancaApplication.class);
+    }
 
 }
