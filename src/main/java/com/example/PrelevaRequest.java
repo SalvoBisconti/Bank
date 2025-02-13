fe77f68661f0247e5e6c9100cbae17000c351975
Salvatore Bisconti <121030864+SalvoBisconti@users.noreply.github.com>
2025-02-13 17:51:17 +0100

    build

--- src/main/java/com/example/PrelevaRequest.java
+++ src/main/java/com/example/PrelevaRequest.java
@@ -2,7 +2,7 @@
 // Questo file è stato generato dall'Eclipse Implementation of JAXB, v3.0.2 
 // Vedere https://eclipse-ee4j.github.io/jaxb-ri 
 // Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2025.02.13 alle 12:47:29 PM CET 
+// Generato il: 2025.02.13 alle 05:50:22 PM CET 
 //
 
 
